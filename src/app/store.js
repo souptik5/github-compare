0e837126546c698fd6730187ac4ed362f25098f5
souptik5 <iamthecubixnerd5@gmail.com>
2020-09-12 04:23:38 +0530

    Bug Fixes

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
-  // reducer: {
-  //   counter: counterReducer,
-  // },
+  reducer: {
+    // counter: counterReducer,
+  },
 });
